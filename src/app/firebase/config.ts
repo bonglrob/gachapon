7029aae3e5b292d0da42a66fa0f9b3e14a2db037
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-03-06 18:03:02 -0800

    chore: hide api keys and add env to gitignore

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDtOK1L7HtDfJ3CPLrR-ecw7zkgcFTV_f4",
-    authDomain: "gachapon-47d45.firebaseapp.com",
-    databaseURL: "https://gachapon-47d45-default-rtdb.firebaseio.com",
-    projectId: "gachapon-47d45",
-    storageBucket: "gachapon-47d45.appspot.com",
-    messagingSenderId: "168171961874",
-    appId: "1:168171961874:web:55f016c877034c614262cb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 // Initialize Firebase
